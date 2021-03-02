a44f39398f6d2c74cc8519840bdfdd31ce19bded
prayas2409 <prayas.hemraj.pagade@gmail.com>
2021-03-02 13:10:42 +0530

    [Prayas] Added: Saving OTP in cache

--- Auth/views.py
+++ Auth/views.py
@@ -6,9 +6,15 @@ from online_assessment.settings import EMAIL_HOST_USER
 from rest_framework import status
 from django.utils.datastructures import MultiValueDictKeyError
 import random
+from online_assessment.settings import REDIS_HOST,REDIS_PASSWORD,REDIS_PORT
+import redis
+from online_assessment.project_cache import Cache
 
 # Create your views here.
 class Login(APIView):
+    
+    def __init__(self):
+        self.cache=Cache(redis.Redis(host=REDIS_HOST,port=REDIS_PORT,password=REDIS_PASSWORD))
 
     def post(self,request):
         """[Post API to get user details and send otp via email]
@@ -23,6 +29,8 @@ class Login(APIView):
             email = request.data['email']
             name = request.data['name']
             otp = random.randint(100000,999999)
+            data = {"otp_{}".format(email):otp}
+            self.cache.set(data)
             send_mail(
             'Account verification',
             '{} is your otp for verification of your account'.format(otp),
@@ -31,10 +39,10 @@ class Login(APIView):
             fail_silently=False,
             )
             response = "Email with OTP has been sent to you on {}".format(name,email)
-
+            print(self.cache.get("otp_{}".format(email)))
             return Response(response,status=status.HTTP_202_ACCEPTED)
         except MultiValueDictKeyError as e:
             Response("Please enter email and name",status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
-            print(type(e))
-            Response("Something went wrong please try again later",status=status.HTTP_400_BAD_REQUEST)
+            print(e)
+            return Response("Something went wrong please try again later",status=status.HTTP_400_BAD_REQUEST)
